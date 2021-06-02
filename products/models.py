bc4064c0504aa18b34af86d631e0c803b6549cc2
aleksei@mazur <mazuralexey@gmail.com>
2021-06-03 01:45:18 +0300

    Create first model, make first migrate

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here. == таблицы
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    description = models.TextField(blank=True)
