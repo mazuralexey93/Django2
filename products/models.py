51c613798dea71df6a208316aac6244c47b10086
aleksei@mazur <mazuralexey@gmail.com>
2021-06-04 13:26:40 +0300

    Create models, edited __str__ , create superuser

--- products/models.py
+++ products/models.py
@@ -4,4 +4,19 @@ from django.db import models
 # Create your models here. == таблицы
 class ProductCategory(models.Model):
     name = models.CharField(max_length=60, unique=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    description = models.CharField(max_length=64, blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
