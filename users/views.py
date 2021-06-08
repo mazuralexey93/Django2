d3db05c9945ac1dc1d0e60a06c602ba9ecae45b3
aleksei@mazur <mazuralexey@gmail.com>
2021-06-08 21:02:40 +0300

    created logic for UserLoginForm

--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
 from users.forms import UserLoginForm
 
 
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))  # '/' - main page
+    else:
+        form = UserLoginForm()
     context = {'title': 'Geekshop - Авторизация', 'form': form}
     return render(request, 'users/login.html', context)
 
