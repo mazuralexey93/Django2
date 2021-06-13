f41adb8aa132a7c0be0ffa61ec504111a973b46b
aleksei@mazur <mazuralexey@gmail.com>
2021-06-13 13:15:38 +0300

    added @login_required

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
@@ -38,6 +39,7 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
+@login_required()
 def profile(request):
     user = request.user
     if request.method == 'POST':
