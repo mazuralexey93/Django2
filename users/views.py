7d5497ae357ed963c6e243c08db0a57626ac4be0
aleksei@mazur <mazuralexey@gmail.com>
2021-06-13 03:11:25 +0300

    added basket app controller

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
+from baskets.models import Basket
 
 
 def login(request):
@@ -30,21 +31,29 @@ def register(request):
             return HttpResponseRedirect(reverse('users:login'))  # '/users/login' - authorization page
     else:
         form = UserRegisterForm()
-    context = {'title': 'Geekshop - Регистрация', 'form': form}
+    context = {
+        'title': 'Geekshop - Регистрация',
+        'form': form,
+    }
     return render(request, 'users/register.html', context)
 
 
 def profile(request):
+    user = request.user
     if request.method == 'POST':
-        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('users:profile'))
         else:
             print(form.errors)  # в дз сделать вывод ошибок и сообщений (success) на странице
     else:
-        form = UserProfileForm(instance=request.user)
-    context = {'title': 'Geekshop - Личный кабинет', 'form': form}
+        form = UserProfileForm(instance=user)
+    context = {
+        'title': 'Geekshop - Личный кабинет',
+        'form': form,
+        'baskets': Basket.objects.filter(user=user),
+    }
     return render(request, 'users/profile.html', context)
 
 
