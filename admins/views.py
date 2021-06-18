f25b292affb5484637a8b2dcf3ce13226a4360bc
aleksei@mazur <mazuralexey@gmail.com>
2021-06-18 20:17:33 +0300

    added admins UPDATE template

--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from users.models import User
-from admins.forms import UserAdminRegisterForm
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
 def index(request):
@@ -22,6 +22,7 @@ def admin_users_create(request):
         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
+            # messages.success(request, 'Пользователь успешно создан')
             return HttpResponseRedirect(reverse('admins:admin_users'))
     else:
         form = UserAdminRegisterForm()
@@ -33,7 +34,21 @@ def admin_users_create(request):
 
 
 def admin_users_update(request, id):
-    return render(request, 'admins/admin_users_update_delete.html')
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_user)
+        if form.is_valid():
+            form.save()
+            # messages.success(request, 'Данные успешно обновлены!')
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'Geekshop|Admin - Обновление пользователя',
+        'form': form,
+        'selected_user': selected_user
+    }
+    return render(request, 'admins/admin_users_update_delete.html', context)
 
 
 def admin_users_delete(request, id):
