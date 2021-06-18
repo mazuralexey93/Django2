28f6c5110843d807bf171d4d0aede7e98ba38c35
aleksei@mazur <mazuralexey@gmail.com>
2021-06-18 19:41:36 +0300

    added admins CREATE template

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegisterForm
 
 
 def index(request):
@@ -8,12 +10,26 @@ def index(request):
 
 
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {
+        'users': User.objects.all(),
+        'title': 'Geekshop|Admin - Пользователи'
+    }
     return render(request, 'admins/admin_users_read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin_users_create.html')
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {
+        'title': 'Geekshop|Admin - Регистрация',
+        'form': form,
+    }
+    return render(request, 'admins/admin_users_create.html', context)
 
 
 def admin_users_update(request, id):
