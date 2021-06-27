2464334fdcd0d7afd9af14a0ecc77e4a23a36738
aleksei@mazur <mazuralexey@gmail.com>
2021-06-27 21:25:21 +0300

    create CBV for admins app

--- admins/urls.py
+++ admins/urls.py
@@ -1,21 +1,22 @@
 from django.urls import path
 
-from admins.views import index, UserListView, UserDeleteView, UserUpdateView, UserCreateView,\
-    UserUnDeleteView, admin_categories, admin_categories_create, admin_categories_update, admin_categories_delete
+from admins.views import UserIndexView, UserListView, UserDeleteView, UserUpdateView, UserCreateView,\
+    UserUnDeleteView, AdminCategoriesListView, AdminCategoriesCreateView, AdminCategoriesUpdateView, AdminCategoryDeleteView
 
 app_name = 'admins'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', UserIndexView.as_view(), name='index'),
+
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users/create', UserCreateView.as_view(), name='admin_users_create'),
     path('users/update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
     path('users/delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete'),
     path('users/undelete/<int:pk>/', UserUnDeleteView.as_view(), name='admin_users_undelete'),
 
-    path('categories/', admin_categories, name='admin_categories'),
-    path('categories/create', admin_categories_create, name='admin_categories_create'),
-    path('categories/update/<int:id>/', admin_categories_update, name='admin_categories_update'),
-    path('categories/delete/<int:id>/', admin_categories_delete, name='admin_categories_delete'),
+    path('categories/', AdminCategoriesListView.as_view(), name='admin_categories'),
+    path('categories/create', AdminCategoriesCreateView.as_view(), name='admin_categories_create'),
+    path('categories/update/<int:pk>/', AdminCategoriesUpdateView.as_view(), name='admin_categories_update'),
+    path('categories/delete/<int:pk>/', AdminCategoryDeleteView.as_view(), name='admin_categories_delete'),
 
 ]
