0d10f4f392015896176443fcba81a95aa8450340
aleksei@mazur <mazuralexey@gmail.com>
2021-07-12 03:05:24 +0300

    reworked a bit

--- authapp/pipeline.py
+++ authapp/pipeline.py
@@ -8,18 +8,21 @@ from social_core.exceptions import AuthForbidden
 
 from authapp.models import UserProfile
 
+
 def save_user_profile(backend, user, response, *args, **kwargs):
     if backend.name != 'vk-oauth2':
         return
 
-    api_url = urlunparse(('https',
-                          'api.vk.com',
-                          '/method/users.get',
-                          None,
-                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'age')),
-                                                access_token=response['access_token'],
-                                                v='5.92')),
-                          None))
+    api_url = urlunparse(
+        ('https',
+         'api.vk.com',
+         '/method/users.get',
+         None,
+         urlencode(OrderedDict(
+             fields=','.join(('bdate', 'sex', 'about', 'age')),
+             access_token=response['access_token'],
+             v='5.92')
+         ), None))
 
     resp = requests.get(api_url)
     if resp.status_code != 200:
