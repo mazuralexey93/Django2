15e4a1ee8a63804bc48d4aa80bf4896122b6a2e4
aleksei@mazur <mazuralexey@gmail.com>
2021-08-21 01:06:10 +0300

    made database schema

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # additional apps
     'debug_toolbar',
     'template_profiler_panel',
+    'django_extensions',
 
     # custom apps
     'social_django',
@@ -180,14 +181,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'geekshop', 'static'),
 )
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     '/var/www/static/',
-# ]
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
