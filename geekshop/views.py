74badedbc7f107a3283bac15f1aa6af1095035b6
aleksei@mazur <mazuralexey@gmail.com>
2021-08-31 19:24:18 +0300

    tests F and Q objects

--- geekshop/views.py
+++ geekshop/views.py
@@ -2,18 +2,11 @@ from django.shortcuts import render
 
 from basketapp.models import Basket
 from mainapp.models import Product
-from ordersapp.models import Order
-from django.views.decorators.cache import cache_page, never_cache
-
-
-def get_data(user):
-    return Order.objects.select_related('user').all()
-
+from django.views.decorators.cache import  never_cache
 
 @never_cache
 def index(request):
 
-    data = get_data(request.user)
     basket = []
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
@@ -23,36 +16,15 @@ def index(request):
 
     context = {
         'mainapp': products,
-        'some_name': 'hello',
         'title': title,
-        'basket': basket,
-        'data': data
+        'basket': basket
     }
     return render(request, 'geekshop/index.html', context)
 
 
-# Кэширование вьюшки декоратором
-@cache_page(300)
 def contacts(request):
     return render(request, 'geekshop/contact.html')
 
 
-from django.core.cache import cache
-from django.conf import settings
-
-
-# Пример использования низкоуровневого кэширования
-def _get_active_products():
-    return Product.objects.select_related().all()
-
-
-def get_product(request):
-    if settings.LOW_CACHE:
-        cached_products = cache.get('products')
-        if cached_products is None:
-            cache.set('products', _get_active_products())
-        return cached_products
-    else:
-        return _get_active_products()
 
 
